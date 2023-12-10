e602ed297482c3b56a965f2f1fb16bacbe571aa0
Emmy Krum <ejkrum@yahoo.com>
2023-12-10 17:44:43 -0500

    deploy update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React from 'react';
 import './App.css';
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
-
 import { layout } from './Layout.js';
 import { redrawCanvas } from './boundary/Boundary.js';
 import { step, generate } from './controller/Controller.js';
